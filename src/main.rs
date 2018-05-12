a614c6ca313bffea601e2b98e325fda4db3531f3
Matthias Bartelmeß <mba@fourplusone.de>
2018-05-12 09:57:19 +0200

    Add to existing feed

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,10 @@ use github::{GithubIssue};
 use linkinfo::{LinkInfo};
 use rss::{Channel, ChannelBuilder, Item, ItemBuilder, Guid};
 use std::env;
+
 use std::fs::File;
+use std::path::Path;
+use std::io::BufReader;
 
 
 
@@ -53,15 +56,34 @@ impl LinkFeed {
     fn build_items(&self) -> Vec<Item> {
         self.links.iter().map(|link| self.build_item(link)).collect()
     }
+
+    fn update_items(&self, items:&[Item]) -> Vec<Item> {
+        let new_items = self.links.iter()
+            .filter(|link| !items.iter().any(|item| item.link() == Some(&link.url)))
+            .map(|link| self.build_item(link));
+
+        let mut combined_items:Vec<Item> = items.iter().map(|item| item.clone()).collect();
+        combined_items.extend(new_items);
+
+        combined_items
+        
+    }
     
-    fn build_rss(&self) -> Channel {
-        ChannelBuilder::default()
-            .title("Channel Title")
-            .link("http://example.com")
-            .description("An RSS feed.")
-            .items(self.build_items())
-            .build()
-            .unwrap()
+    fn build_rss(&self, channel:Option<Channel>) -> Channel {
+        match channel {
+            Some(mut channel) => {
+                let items = self.update_items(channel.items());
+                channel.set_items(items); 
+                channel
+            }
+            None => ChannelBuilder::default()
+                .title("Channel Title")
+                .link("http://example.com")
+                .description("An RSS feed.")
+                .items(self.build_items())
+                .build()
+                .unwrap()
+        }
     }
 }
 
@@ -77,6 +99,17 @@ fn main() {
     let url_fragment = &args[1];
     let rss_file_name = &args[2];
     
+    let rss_file_path = Path::new(rss_file_name);
+
+    let channel:Option<Channel>;
+
+    if rss_file_path.exists() {
+        let f = File::open(rss_file_name).expect("Cannot open file");
+        channel = Some(Channel::read_from(BufReader::new(f)).unwrap());
+    } else {
+        channel = None;
+    }
+
     let issue_url = format!("{}/{}", BASE_ISSUE_URL, url_fragment);
     
     let issue = GithubIssue::get(&issue_url).unwrap().get_comments().unwrap();
@@ -89,7 +122,7 @@ fn main() {
     
     let link_feed = LinkFeed::new(&links);
     
-    let channel = link_feed.build_rss();
+    let channel = link_feed.build_rss(channel);
     
     let file = File::create(rss_file_name).unwrap();
     channel.write_to(file).unwrap(); // write to the channel to a writer
